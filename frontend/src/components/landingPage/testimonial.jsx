c219eadc52754224584013574b386ef6c591c7dc
Moses-main <moseschizaram8@gmail.com>
2025-02-15 01:23:19 +0100

    Add controls to the carousel

--- frontend/src/components/landingPage/testimonial.jsx
+++ frontend/src/components/landingPage/testimonial.jsx
@@ -1,9 +1,15 @@
 import { Carousel } from "flowbite-react";
+import { FaChevronCircleRight } from "react-icons/fa";
+import { FaCircleChevronLeft } from "react-icons/fa6";
 
 export default function Testimonial() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel>
+      <Carousel
+        pauseOnHover
+        leftControl={<FaCircleChevronLeft />}
+        rightControl={<FaChevronCircleRight />}
+      >
         <div className="max-w-md border bg-[#F3F3F3] backdrop-blur-md border-1 px-2 rounded-md">
           <h2 className="font-bold text-center py-3 ">Client Testimonial</h2>
           <p className="text-sm  text-center">
